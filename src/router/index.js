cb63cad7f63ccfebccdbcc3cebcda6b0f419de02
juli6464 <juli6464@hotmail.com>
2023-10-02 14:24:00 -0500

    guardas de navegacion

--- src/router/index.js
+++ src/router/index.js
@@ -54,4 +54,12 @@ const router = createRouter({
 
   ],
 });
+
+router.beforeEach((to, from) => {
+  console.log(to,from)
+
+  if(to.path === '/') return { name: 'about' }
+  return true
+})
+
 export default router;
